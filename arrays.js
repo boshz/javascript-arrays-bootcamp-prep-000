2bbb43dc1c5cd5f5755cf60c56e06cf4ee7aa3f6
JOSHUA ZYNDORF <zyndorf.1@osu.edu>
2018-01-26 22:09:50 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,5 +2,5 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array, element) {
   array = [...element, 1]
-  return array
+  console.log(array)
 }
